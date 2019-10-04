2d66070d24d891d0e4d4386bb712b22ea23208df
Mario Renteria <mrente94@gmail.com>
2019-10-04 14:01:46 -0700

    boiler plate

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import React, { Component } from "react";
+import pics from "./pictures.json"
+
+class App extends React.Component {
+  state = {
+    
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <Navbar />
+      </div>
+    )
+  }    
+}
+
+export default App;
